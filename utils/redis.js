9ed475473b6d90dfd8883cc5d78895b52357b3a5
Justin Thurman <1828@holbertonschool.com>
2022-04-30 14:22:43 -0500

    I'm still not solid on what all redis is doing on my machine but the output was correct.)

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (err) => {
+      const Error = err;
+      const mess = 'Redis Client not connected to the server:';
+      const message = mess + Error;
+      console.log(message);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
